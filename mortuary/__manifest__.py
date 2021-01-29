39eacb43b33da14b45ca05137bb595095dda5fa2
Eduardo Serrano <eduardo.serrano@pabsmr.org>
2021-01-29 15:02:40 -0600

    [FIX] change depends

--- mortuary/__manifest__.py
+++ mortuary/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'mail', 'hr', 'charge_collectors'],
+    'depends': ['base', 'mail', 'hr', 'pabs_custom'],
 
     # always loaded
     'data': [
