68dbad6ed5d50ef60e8272e7eec4c31273469d74
Eduardo Serrano <eduardo.serrano@pabsmr.org>
2021-02-04 11:35:09 -0600

    [FIX] creación de arbol de comisiones

--- pabs_custom/inherits/hr_employee.py
+++ pabs_custom/inherits/hr_employee.py
@@ -381,8 +381,8 @@ class HrEmployee(models.Model):
     ### Sí el campo modificado hace referencia a ventas, debe crear arbol de comisiones
     if vals.get('job_id'):
       job_ids = self.env['hr.job'].search([
-        '|',('name','=','ASISTENTE SOCIAL')
-        ('name','=','COORDINADOR')
+        '|',('name','=','ASISTENTE SOCIAL'),
+        ('name','=','COORDINADOR'),
         ('name','=','GERENTE DE OFICINA')])
       raise ValidationError((
         "Valores retornados: {}".format(job_ids.ids)))
