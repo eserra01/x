718fb988dc0e26f6886f7065a4ee325a171615f4
Eduardo Serrano <eduardo.serrano@pabsmr.org>
2021-02-08 11:26:22 -0600

    [FIX] create hr employe

--- pabs_custom/inherits/stock_move.py
+++ pabs_custom/inherits/stock_move.py
@@ -379,6 +379,15 @@ class StockMove(models.Model):
 
   @api.model
   def create(self, vals):
+    pricelist_item_obj = self.env['product.pricelist.item']
+    product_obj = self.env['product.product']
+    if vals.get('product_id'):
+      product_id = product_obj.browse(vals.get('product_id'))
+      if product_id.tracking == 'serial':
+        item_id = pricelist_item_obj.search([('product_id','=',product_id.id)],
+          order="create_date desc",limit=1)
+        if item_id:
+          vals['papeleria'] = item_id.stationery
     res = super(StockMove, self).create(vals)
     picking_obj = self.env['stock.picking']
     move_line_obj = self.env['stock.move.line']
