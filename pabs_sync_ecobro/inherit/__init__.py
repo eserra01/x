6e17175c985742bbdeab1eb829f5abb7fb2060fc
Eduardo Serrano <eduardo.serrano@pabsmr.org>
2021-02-23 16:40:01 -0600

    [FIX] NC

--- pabs_sync_ecobro/inherit/__init__.py
+++ pabs_sync_ecobro/inherit/__init__.py
@@ -4,3 +4,4 @@ from . import ecobro_settings
 from . import hr_employee
 from . import pabs_contract
 from . import account_payment
+from . import account_move
---
+++ pabs_sync_ecobro/inherit/account_move.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+import logging
+
+
+_logger = logging.getLogger(__name__)
+
+class AccountMove(models.Model):
+  _inherit = 'account.move'
+
+  def invert_refund_invoices(self):
+    account_move_obj = self.env['account.move']
+    move_ids = account_move_obj.search([
+      ('type','=','out_refund'),
+      ('state','=','posted'),
+      ('invoice_payment_ref','!=',False)])
+    move_ids.sorted(key=lambda r: r.invoice_payment_ref)
+    last = False
+    for move_id in move_ids:
+      _logger.warning("El recibo: {} se esta procesando".format(move_id.invoice_payment_ref))
+      if move_id.invoice_payment_ref != last:
+        last = move_id.invoice_payment_ref
+        continue
+      else:
+        move_id.button_draft()
+        move_id.button_cancel()
