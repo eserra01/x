744883bb19f14aaad0c2b0a13c2a3608e3f2235b
Eduardo Serrano <eduardo.serrano@pabsmr.org>
2021-02-25 12:31:35 -0600

    [FIX] se agrega método para crear salidas a los pagos

--- pabs_sync_ecobro/inherit/account_payment.py
+++ pabs_sync_ecobro/inherit/account_payment.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 class AccountPayment(models.Model):
   _inherit = 'account.payment'
 
   ecobro_affect_id = fields.Char(string='Afectación de ecobro ID')
+
+  def generate_output_comission(self):
+    comission_tree_obj = self.env['pabs.comission.tree']
+    payment_obj = self.env['account.payment']
+    payment_ids = payment_obj.search([
+      ('ecobro_affect_id','!=',False),
+      ('comission_output_ids','=',False)])
+    raise ValidationError((
+      "Registros encontrados: {}".format(len(payment_ids))))
+    comission_tree_obj.CrearSalidas(
+      IdPago=IdPago, NumeroContrato=NumeroContrato,
+      CodigoCobrador=CodigoCobrador, MontoPago=MontoPago,
+      EsExcedente=False)
