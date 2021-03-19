4dfab33274e137f61627cc8a5c4a488de3441b1c
Eduardo Serrano <eduardo.serrano@pabsmr.org>
2021-03-19 15:30:27 -0600

    [FIX] no se elimina la linea de comision

--- pabs_custom/models/pabs_comission_tree.py
+++ pabs_custom/models/pabs_comission_tree.py
@@ -274,12 +274,7 @@ class ComissionTree(models.Model):
 
             #Si es cobrador y se quedó sin comisión: eliminar el registro del árbol. De lo contrario, actualizar.
             if com.job_id.id == id_cargo_cobrador:
-                if comisionRealPagada <= 0:
-                    try:
-                        com.unlink()
-                    except Exception as e:
-                        _logger.warning(e)
-                else:
+                if comisionRealPagada > 0:
                     com.write({"actual_commission_paid":comisionRealPagada})
             else:
                 com.write({"commission_paid":comisionPagada, "actual_commission_paid":comisionRealPagada, "remaining_commission":comisionRestante})
