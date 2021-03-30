542a0a77abb664e6645a6eda6bf9942cb75a1cb9
Eduardo Serrano <eduardo.serrano@pabsmr.org>
2021-03-30 09:20:57 -0600

    [ADD] se agrega validación para crear empleados

--- pabs_custom/inherits/hr_employee.py
+++ pabs_custom/inherits/hr_employee.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models, api
-from odoo.exceptions import ValidationError
+from odoo.exceptions import ValidationError 
 
 OPTIONS = [
   ('true','Si'),
