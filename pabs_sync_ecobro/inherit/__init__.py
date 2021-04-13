b7d9c5c95b5943c65e950dbee2ce2bd8b9e92943
Eduardo Serrano <eduardo.serrano@pabsmr.org>
2021-04-13 11:30:30 -0500

    [FIX]

--- pabs_sync_ecobro/inherit/__init__.py
+++ pabs_sync_ecobro/inherit/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 #from . import ecobro_settings
+from . import res_company
 from . import hr_employee
 from . import pabs_contract
 from . import account_payment
 from . import account_move
 from . import pabs_contract_status
-from . import res_company
