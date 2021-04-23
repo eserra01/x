db521caed91388871689e315306c9ab7875df4bd
Eduardo Serrano <eduardo.serrano@pabsmr.org>
2021-04-23 10:47:27 -0500

    [FIX] product multicompany

--- pabs_custom/inherits/__init__.py
+++ pabs_custom/inherits/__init__.py
@@ -14,3 +14,4 @@ from . import account_move
 from . import account_payment
 from . import res_config_settings
 from . import res_partner
+from . import product_template
---
+++ pabs_custom/inherits/product_template.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models, api
+
+class ProductTemplate(models.Model):
+  _inherit = 'product.template'
+
+  company_id = fields.Many2one(comodel_name='product.template',
+    default=lambda s: s.env.company.id, index=True)
