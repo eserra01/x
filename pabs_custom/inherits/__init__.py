c8680a547582372a421d7655f91115c864ddcc6a
Eduardo Serrano <eduardo.serrano@pabsmr.org>
2021-04-23 10:52:33 -0500

    [FIX]

--- pabs_custom/inherits/__init__.py
+++ pabs_custom/inherits/__init__.py
@@ -14,4 +14,3 @@ from . import account_move
 from . import account_payment
 from . import res_config_settings
 from . import res_partner
-from . import product_template
--- pabs_custom/inherits/product_template.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models, api
-
-class ProductTemplate(models.Model):
-  _inherit = 'product.template'
-
-  company_id = fields.Many2one(comodel_name='product.template',
-    default=lambda s: s.env.company.id, index=True) 
