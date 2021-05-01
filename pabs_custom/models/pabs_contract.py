104f5f3691443e1e91f136b607160dab8cf13c04
Eduardo Serrano <eduardo.serrano@pabsmr.org>
2021-05-01 04:46:10 -0500

    [LOG]

--- pabs_custom/models/pabs_contract.py
+++ pabs_custom/models/pabs_contract.py
@@ -924,7 +924,6 @@ class PABSContracts(models.Model):
               'exclude_from_invoice_tab' : False,
               'name' : product_id.description_sale or product_id.name,
             }
-            raise ValidationError("datos: {}".format(line_data))
             line = account_line_obj.create(line_data)
             ### CONTRAPARTIDA DEL DOCUMENTO
             partner_line_data = {
