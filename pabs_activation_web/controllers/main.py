fcf33bfd2414db28cc59997b61cf5b3b1025a1a8
Eduardo Serrano <eduardo.serrano@pabsmr.org>
2021-06-10 10:37:39 -0500

    [FIX] multi compañia en activaciones

--- pabs_activation_web/controllers/main.py
+++ pabs_activation_web/controllers/main.py
@@ -144,7 +144,7 @@ class ActivationWeb(http.Controller):
         'neighborhood_id' : int(kargs.get('neighborhood_id')),
         'company_id' : company_id
       })
-    contract_id = contract_obj.with_user(request.env.context['uid']).with_context({
+    contract_id = contract_obj.with_user(request.env.context['uid']).with_context(force_company=company_id,{
       'company_id' : company_id}).create(kargs)
     if contract_id:
       response = {'result' : {'activation_code' : contract_id.activation_code}}
