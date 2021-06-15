7cb27d171b271c1e74142924d6ff69936f419e8c
Eduardo Serrano <eduardo.serrano@pabsmr.org>
2021-06-15 11:31:57 -0500

    [FIX] plantilla de produccion de un asistente

--- xmarts_funeraria/wizard/pabs_production_promoter.py
+++ xmarts_funeraria/wizard/pabs_production_promoter.py
@@ -57,15 +57,77 @@ class PABSProductionPromoterXLSX(models.AbstractModel):
     title_format = workbook.add_format({'bold' : True, 'font_size' : 14, 'center_across' : True})
     subtitle_format = workbook.add_format({'font_size' : 12, 'center_across' : True})
     bold_format = workbook.add_format({'bold' : True})
-    header_format = workbook.add_format({'bold' : True,'bg_color': '#2978F8'})
+    header_format = workbook.add_format({'bold' : True,'bg_color': '#76A8F9'})
     money_format = workbook.add_format({'num_format': '$#,##0.00'})
     subtotal_format = workbook.add_format({'top' : 2, 'bold' : True, 'num_format': '$#,##0.00'})
 
+    ### BUSCAMOS LOS CONTRATOS DE ESE ASISTENTE
+    contract_ids = contract_obj.search([
+      ('state' , '=', 'contract'),
+      ('sale_employee_id','=',data.get('employee_id'))])
+
+
     ### ESCRIBIMOS EL ENCABEZADO DE LA PAGINA
-    sheet.merge_range("A2:D2","Programa de Apoyo en Beneficio Social", title_format)
-    sheet.merge_range("A3:D3", "Producción de asistente", subtitle_format)
-    sheet.merge_range("A4:B4", "{} - {}".format(employee_id.barcode, employee_id.name))
+    sheet.merge_range("A2:R2","Programa de Apoyo en Beneficio Social", title_format)
+    sheet.merge_range("A3:R3", "Producción de asistente", subtitle_format)
+    sheet.merge_range("A4:R4", "{} - {}".format(employee_id.barcode, employee_id.name))
 
     for index, val in enumerate(HEADERS):
       sheet.write(5,index,val,header_format)
+
+    ### CONTADOR
+    count = 6
+
+    ### RECORREMOS LOS CONTRATOS
+    for contract_id in contract_ids:
+      ### CÓDIGO DEL PROMOTOR
+      sheet.write(count, 1, contract_id.sale_employee_id.barcode)
+      ### NOMBRE DEL PROMOTOR
+      sheet.write(count, 2, contract_id.sale_employee_id.name)
+      ### OFICINA
+      sheet.write(count, 3, contract_id.warehouse_id.name or '')
+      ### ESTATUS DEL PROMOTOR
+      sheet.write(count, 4, contract_id.sale_employee_id.employee_status.name or '')
+      ### TIPO DE INGRESO
+      sheet.write(count, 6, contract_id.payment_scheme_id.name or '')
+      ### FECHA DE ELABORACIÓN
+      sheet.write(count, 7, contract_id.invoice_date or '')
+      ### CONTRATO
+      sheet.write(count, 8, contract_id.name or '')
+      ### COSTO
+      sheet.write(count, 9, contract_id.product_price or 0)
+      ### SALDO
+      sheet.write(count, 10, contract_id.balance or 0)
+      ### CLIENTE
+      sheet.write(count, 11, contract_id.full_name or '')
+      street = ''
+      neightborhood = ''
+      municipality = ''
+      phone = ''
+      if contract_id.street_name_toll:
+        street = "{} {}".format(
+          contract_id.street_name_toll or '', contract_id.street_number_toll or '')
+        neightborhood = contract_id.toll_colony_id.name
+        municipality = contract_id.toll_municipallity_id.name
+        phone = contract_id.phone_toll
+      elif contract_id.street_name:
+        street = "{} {} {}".format(
+          contract_id.street_name or '', contract_id.street_number or '')
+        neightborhood = contract_id.neighborhood_id.name
+        municipality = contract_id.municipality_id.name
+        phone = contract_id.phone
+      ### DOMICILIO
+      sheet.write(count, 12, street)
+      ### COLONIA
+      sheet.write(count, 13, neightborhood)
+      ### MUNICIPIO
+      sheet.write(count, 14, municipality)
+      ### TELÉFONO
+      sheet.write(count, 15, phone)
+      ### ESTATUS
+      sheet.write(count, 16, contract_id.contract_status_item.status or '')
+      ### MOTIVO
+      sheet.write(count, 17, contract_id.contract_status_reason.status_id or '')
+      ### COBRADOR
+      sheet.write(count, 18, contract_id.debt_collector.name or '')
 
