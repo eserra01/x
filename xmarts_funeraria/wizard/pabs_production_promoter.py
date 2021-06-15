5ec88d194c4b88a8e45769b86103701763230ee2
Eduardo Serrano <eduardo.serrano@pabsmr.org>
2021-06-15 16:37:55 -0500

    [FIX] plantilla de produccion de un asistente

--- xmarts_funeraria/wizard/pabs_production_promoter.py
+++ xmarts_funeraria/wizard/pabs_production_promoter.py
@@ -20,8 +20,33 @@ HEADERS = [
   'Estatus',
   'Motivo',
   'Cobrador',
-  'Recomendado'
-  ### VALIDAR CONCEPTOS DE DE ARBOL DE COMISIONES
+  ### RECOMENDADO //
+  'Recomendado / N/A',
+  'Comision correspondiente',
+  'Comision restante',
+  ### PROMOTOR //
+  'Promotor que comisiona',
+  'Comission correspondiente',
+  'Comision restante',
+  ### COORDINADOR // GERENTE JR
+  'Coordinador / Gerente Jr',
+  'Comisionista',
+  'Comision correspondiente',
+  'Comision Restante',
+  ### GERENTE // GERENTE SR
+  'Gerente / Gerente Sr',
+  'Comisionista',
+  'Comision correspondiente',
+  'Comision restante',
+  ### SI ES ALEX DEY QUE APLIQUE PRESIDENTE
+  'N/A  / Presidente',
+  'Comisionista',
+  'Comision correspondiente',
+  'Comision restante',
+
+  'Fecha de primer abono',
+  'Observaciones',
+  
 ]
 
 class PabsProductionPromoter(models.TransientModel):
@@ -46,6 +71,7 @@ class PABSProductionPromoterXLSX(models.AbstractModel):
     ### DECLARAMOS OBJETOS
     contract_obj = self.env['pabs.contract']
     employee_obj = self.env['hr.employee']
+    comment_obj = self.env['pabs.contract.comments']
 
     ### INSTANCIAMOS EL OBJETO DEL EMPLEADO
     employee_id = employee_obj.browse(data.get('employee_id'))
@@ -118,17 +144,96 @@ class PABSProductionPromoterXLSX(models.AbstractModel):
         municipality = contract_id.municipality_id.name
         phone = contract_id.phone
       ### DOMICILIO
-      sheet.write(count, 10, street)
+      sheet.write(count, 10, street or '')
       ### COLONIA
-      sheet.write(count, 11, neightborhood)
+      sheet.write(count, 11, neightborhood or '')
       ### MUNICIPIO
-      sheet.write(count, 12, municipality)
+      sheet.write(count, 12, municipality or '')
       ### TELÉFONO
-      sheet.write(count, 13, phone)
+      sheet.write(count, 13, phone or '')
       ### ESTATUS
       sheet.write(count, 14, contract_id.contract_status_item.status or '')
       ### MOTIVO
       sheet.write(count, 15, contract_id.contract_status_reason.reason or '')
       ### COBRADOR
       sheet.write(count, 16, contract_id.debt_collector.name or '')
-      count += 1
+      ### FILTRAMOS EL ARBOL DE COMISIONES
+      comission_tree = contract_id.commission_tree
+      ### BUSCAMOS SI EXISTE RECOMENDADO
+      recomended = comission_tree.filtered(lambda r: r.job_id.name == 'RECOMENDADO')
+      ### SI EXISTE RECOMENDADO, ESCRIBIMOS DATOS
+      if recomended:
+        sheet.write(count, 17, "RECOMENDADO")
+        sheet.write(count, 18, recomended.corresponding_commission or 0, money_format)
+        sheet.write(count, 19, recomended.remaining_commission or 0, money_format)
+      ### SI NO, ENVIAMOS QUE NO APLICA
+      else:
+        sheet.write(count, 17, "N/A")
+        sheet.write(count, 18, 0, money_format)
+        sheet.write(count, 19, 0, money_format)
+      ### AGREGAMOS PROMOTOR
+      promoter = comission_tree.filtered(lambda r: r.job_id.name == 'ASISTENTE SOCIAL')
+      ### SI SE ENCUENTRA EL PROMOTOR
+      if promoter:
+        ### ESCRIBIMOS PROMOTOR QUE COMISIONA
+        sheet.write(count, 20, promoter.comission_agent_id.name)
+        sheet.write(count, 21, promoter.corresponding_commission or 0, money_format)
+        sheet.write(count, 22, promoter.remaining_commission or 0, money_format)
+      ### SI NO EXISTE PROMOTOR
+      else:
+        sheet.write(count, 20, "N/A")
+        sheet.write(count, 21, 0, money_format)
+        sheet.write(count, 22, 0, money_format)
+      ### BUSCAMOS SI HAY UN COORDINADOR O UN GERENTE JR
+      coordinator = comission_tree.filtered(lambda r: r.job_id.name in ('COORDINADOR','GERENTE JR'))
+      ### SI ENCONTRAMOS COORDINADOR O UN GERENTE JR
+      if coordinator:
+        sheet.write(count, 23, coordinator.job_id.name)
+        sheet.write(count, 24, coordinator.comission_agent_id.name)
+        sheet.write(count, 25, coordinator.corresponding_commission or 0, money_format)
+        sheet.write(count, 26, coordinator.remaining_commission or 0, money_format)
+      ### SI NO
+      else:
+        sheet.write(count, 23, "N/A")
+        sheet.write(count, 24, "N/A")
+        sheet.write(count, 25, 0, money_format)
+        sheet.write(count, 26, 0, money_format)
+      ### BUSCAMOS SI HAY UN GERENTE
+      gerent = comission_tree.filtered(lambda r: r.job_id.name in ('GERENTE','GERENTE SR'))
+      ### SI ENCONTRAMOS GERENTE O GERENTE SR
+      if gerent:
+        sheet.write(count, 27, gerent.job_id.name)
+        sheet.write(count, 28, gerent.comission_agent_id.name)
+        sheet.write(count, 29, gerent.corresponding_commission or 0, money_format)
+        sheet.write(count, 30, gerent.remaining_commission or 0, money_format)
+      ### SI NO
+      else:
+        sheet.write(count, 27, "N/A")
+        sheet.write(count, 28, "N/A")
+        sheet.write(count, 29, 0, money_format)
+        sheet.write(count, 30, 0, money_format)
+      ### BUSCAMOS PUESTO DE PRESIDENTE
+      president = comission_tree.filtered(lambda r: r.job_id.name == 'PRESIDENTE')
+      ### SI ENCONTRAMOS PRESIDENTE (ALEX DEY)
+      if president:
+        sheet.write(count, 31, president.job_id.name)
+        sheet.write(count, 32, president.comission_agent_id.name)
+        sheet.write(count, 33, president.corresponding_commission or 0, money_format)
+        sheet.write(count, 34, president.remaining_commission or 0, money_format)
+      ### SI NO
+      else:
+        sheet.write(count, 27, "N/A")
+        sheet.write(count, 28, "N/A")
+        sheet.write(count, 29, 0, money_format)
+        sheet.write(count, 30, 0, money_format)
+      ### FECHA DE PRIMER ABONO
+      sheet.write(count, 31, contract_id.date_first_payment, date_format)
+      ### COMENTARIOS
+      comment_ids = comment_obj.search([('contract_id','=',contract_id.id)])
+      comment = ''
+      for line in comment_ids.mapped('comment'):
+        comment = comment + line + "\n"
+      sheet.write(count, 32, comment)
+
+
+
