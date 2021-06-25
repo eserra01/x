ec3574edf126b9ec0dad315e36ac384660d2b0a1
Eduardo Serrano <eduardo.serrano@pabsmr.org>
2021-06-25 13:53:33 -0500

    [FIX] corrección de contratos

--- pabs_custom/models/pabs_contract.py
+++ pabs_custom/models/pabs_contract.py
@@ -595,7 +595,7 @@ class PABSContracts(models.Model):
         if not received_contract:
           raise ValidationError((
             "No se encuentra la ubicación de contratos"))
-        if received_contract != location_id:
+        if received_contract.id != location_id.id:
           raise ValidationError((
             "la solicitud {} no se encontró en la ubicación de contratos, se encuentra en {}".format(self.lot_id.name, location_id.name_get()[0][1])))
         contract = self.search([
