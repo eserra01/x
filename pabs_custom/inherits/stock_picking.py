0193832ffbf35bb24b76d4ba927fac482d90f648
Eduardo Serrano <eduardo.serrano@pabsmr.org>
2021-07-02 09:52:47 -0500

    [FIX] transfers

--- pabs_custom/inherits/stock_picking.py
+++ pabs_custom/inherits/stock_picking.py
@@ -180,9 +180,8 @@ class StockPicking(models.Model):
       for line in picking.move_line_ids_without_package:
         lot_id = line.lot_id
         quant_ids = quant_obj.search([
-          ('inventory_quantity','>',0),
           ('lot_id','=',lot_id.id)]).filtered(
-          lambda r: r.location_id.usage == 'internal')
+          lambda r: (r.location_id.usage == 'internal' and r.inventory_quantity > 0))
         if len(quant_ids) > 1:
           raise ValidationError("No se puede transferir porque la solicitud {} se encuentra en {} lugares diferentes".format(line.lot_id.name, len(quant_ids)))
         else:
