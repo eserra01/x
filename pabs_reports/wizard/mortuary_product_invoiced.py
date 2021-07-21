efe9049e0a3627e8bf4843cd80faf35888cb8036
Eduardo Serrano <eduardo.serrano@pabsmr.org>
2021-07-21 15:20:50 -0500

    [ADD] se agrega la posibilidad de exportar el reporte de facturación de funeraria

--- pabs_reports/wizard/mortuary_product_invoiced.py
+++ pabs_reports/wizard/mortuary_product_invoiced.py
@@ -3,6 +3,11 @@
 from odoo import fields, models, api
 from odoo.exceptions import ValidationError
 
+HEADERS = [
+  'Concepto',
+  'Cantidad',
+  'Total']
+
 class MortuaryProductInvoiced(models.TransientModel):
   _name = 'mortuary.product.invoiced'
 
@@ -11,7 +16,7 @@ class MortuaryProductInvoiced(models.TransientModel):
 
   end_date = fields.Date(string='Fecha Final')
 
-  def print_pdf_report(self):
+  def print_report(self):
     ### DECLARACIÓN DE OBJETOS
     invoice_obj = self.env['account.move']
     ### DECLARAMOS VARIABLE DOMINIO
@@ -40,8 +45,11 @@ class MortuaryProductInvoiced(models.TransientModel):
       'name' : name,
       'invoice_ids' : invoice_ids.ids}
 
-    ### RETORNAMOS EL REPORTE
-    return self.env.ref('pabs_reports.mortuary_product_invoiced_report').report_action(self, data=data)
+    if self._context.get('type_report') == 'pdf':
+      ### RETORNAMOS EL REPORTE EN PDF
+      return self.env.ref('pabs_reports.mortuary_product_invoiced_report').report_action(self, data=data)
+    elif self._context.get('type_report') == 'xlsx':
+      return self.env.ref('pabs_reports.mortuary_product_invoiced_report_xlsx').report_action(self, data=data)
 
 class MortuaryProductInvoicedPDFReport(models.AbstractModel):
   ### TEMPLATE
@@ -88,4 +96,51 @@ class MortuaryProductInvoicedPDFReport(models.AbstractModel):
       'name' : data.get('name'),
       'detail' : details,
     }
-    
+
+class MortuaryProductInvoicedPDFReport(models.AbstractModel):
+  _name = 'report.pabs_reports.mortuary_product_invoiced_xlsx'
+  _inherit = 'report.report_xlsx.abstract'
+
+  def generate_xlsx_report(self, workbook, data, lines):
+    ### INSTANCIACIÓN DE OBJETOS
+    invoice_obj = self.env['account.move']
+
+    ### MENSAJE DE ERROR
+    if not data.get('invoice_ids'):
+      raise ValidationError("Error!")
+
+    ### INSTANCIAMOS LAS FACTURAS GENERADAS
+    invoice_ids = invoice_obj.browse(data.get('invoice_ids'))
+
+    ### GENERAMOS LA HOJA
+    sheet = workbook.add_worksheet(data.get('name'))
+
+    ### AGREGAMOS FORMATOS
+    bold_format = workbook.add_format({'bold': True,'bg_color': '#2978F8'})
+    money_format = workbook.add_format({'num_format': '$#,##0.00'})
+
+     ### INSERTAMOS LOS ENCABEZADOS
+    for row, row_data in enumerate(HEADERS):
+      sheet.write(0,row,row_data,bold_format)
+
+    ### TRAEMOS TODAS LAS LINEAS DE LAS FACTURAS
+    lines = invoice_ids.mapped('invoice_line_ids')
+    ### LISTAMOS TODOS LOS PRODUCTOS FACTURADOS
+    product_ids =  lines.mapped('product_id')
+
+    ### CONTADOR DE REGISTROS
+    count = 1
+
+    ### RECORREMOS LA LISTA DE PRODUCTOS
+    for product_id in product_ids:
+      ### FILTRAMOS TODAS LAS LINEAS QUE VIENE EL PRODUCTO
+      product_lines = lines.filtered(lambda r: r.product_id.id == product_id.id)
+      ### SUAMMOS TODAS LAS CANTIDADES DE ESE PRODUCTO
+      qty = sum(product_lines.mapped('quantity'))
+      ### SUMAMOS EL TOTAL FACTURADO POR ESE PRODUCTO
+      total = sum(product_lines.mapped('price_total'))
+
+      sheet.write(count, 0, product.name)
+      sheet.write(count, 1, int(qty))
+      sheet.write(count, 2, total, money_format)
+      count+= 1
