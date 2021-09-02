9056bcff3b14e81f22a9f6a6e606eb27734fe0ad
Jaime Rodriguez <eduardo.serrano@pabsmr.org>
2021-09-02 17:05:21 -0500

    [FIX]

--- pabs_activation_web/controllers/main.py
+++ pabs_activation_web/controllers/main.py
@@ -12,7 +12,7 @@ class ActivationWeb(http.Controller):
   @http.route('/main/activation', auth="user", type="http")
   def get_main_page(self):
     company_obj = request.env['res.company'].sudo()
-    company_ids = company_obj.search([])
+    company_ids = company_obj.search([('company_id','not in','(9,10)')])
     values = []
     for company_id in company_ids:
       values.append('<option value="{}">{}</option>'.format(company_id.id, company_id.name))
