6f8870366a390e925e6f82a7db17c394b6048e7f
Mauricio Ruiz <mauricio.ruiz@experts.com.mx>
2022-03-15 19:31:00 -0600

    [IMP] Done state at payment post

--- pabs_hr_expense_custom/models/account_payment.py
+++ pabs_hr_expense_custom/models/account_payment.py
@@ -13,3 +13,18 @@ class AccountPayment(models.Model):
   _inherit = 'account.payment'
 
   expense_sheet_id = fields.Many2one(comodel_name="hr.expense.sheet", string="Gasto")
+
+  
+  def cancel(self):    
+    res = super(AccountPayment, self).cancel()
+    # Si el pago está sociado a un gasto
+    if self.expense_sheet_id:
+      # Si el monto del pago es mayor o igual al gasto
+      if self.amount >= self.expense_sheet_id.total_amount:
+        # Se cancela el gasto
+        self.expense_sheet_id.state = 'cancel'
+        expense_id = self.env['hr.expense'].search([('sheet_id','=',self.expense_sheet_id.id)])
+        if expense_id:
+          expense_id.state = 'refused'
+    return res
+      
