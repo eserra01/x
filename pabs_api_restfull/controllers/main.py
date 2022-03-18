57329dccf8206b7efb5a7f126a08502be8266fa9
jaimerodriguezpabs <jaime.rodriguez@pabsmr.org>
2022-03-18 10:51:36 -0600

    [FIX] Se agrega query de financieros con parÃ¡metros que sustituye los anteriores

--- pabs_api_restfull/controllers/main.py
+++ pabs_api_restfull/controllers/main.py
@@ -882,12 +882,18 @@ class APIREST(http.Controller):
 
 #  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
 
-  @http.route('/api/get/financieros_1/<int:company_id>', type='http', auth='none', csrf=False)
-  def get_financial_1(self, **kargs):
+  @http.route('/api/get/financieros_param/<int:company_id>/<int:year>/<int:month>', type='http', auth='none', csrf=False)
+  def get_financial_param(self, **kargs):
     if not kargs.get('company_id'):
-      return Response("Necesitas enviar un parametro de busqueda", status=400)
+      return Response("No se definió una compañia", status=400)
+    elif not kargs.get('year'):
+      return Response("No se definió un año", status=400)
+    elif not kargs.get('month'):
+      return Response("No se definió un mes", status=400)
     else:
       company_id = kargs.get('company_id')
+      year = kargs.get('year')
+      month = kargs.get('month')
       company = request.env['res.company'].browse(company_id)
     response_header = {'Content-Type': 'application/json'}
     cr = request.cr
@@ -1072,229 +1078,10 @@ class APIREST(http.Controller):
       LEFT JOIN account_account as acc on mov.account_id = acc.id
       LEFT JOIN account_analytic_account as ana on mov.analytic_account_id = ana.id
         WHERE enc.state = 'posted'
+        AND DATE_PART('year', enc.date) = {}
+        AND DATE_PART('month', enc.date) = {}
         AND enc.company_id = {}
-        AND DATE_PART('year', enc.date) = 2021 
-        AND DATE_PART('month', enc.date) < 6
-    ) as financieros""".format(company.name, company_id)
-    try:
-      cr.execute(query)
-      records = []
-      headers = [d[0] for d in cr.description]
-      for res in cr.fetchall():
-        data = {}
-        for ind, rec in enumerate(headers):
-          if isinstance(res[ind], datetime.date):
-            value = res[ind].strftime("%d/%m/%Y")
-          else:
-            value = res[ind]
-          data.update({
-            headers[ind] : value
-          })
-        records.append(data)
-      response = {
-        'result' : records
-      }
-      return Response(json.dumps(response),headers=response_header)
-    except Exception as e:
-      return str(e)
-    return Response("Petición Denegada", status=400)
-
-#  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
-
-  @http.route('/api/get/financieros_2/<int:company_id>', type='http', auth='none', csrf=False)
-  def get_financial_2(self, **kargs):
-    if not kargs.get('company_id'):
-      return Response("Necesitas enviar un parametro de busqueda", status=400)
-    else:
-      company_id = kargs.get('company_id')
-      company = request.env['res.company'].browse(company_id)
-    response_header = {'Content-Type': 'application/json'}
-    cr = request.cr
-    query = """
-    SELECT 
-      Grupo, 
-      Plaza, 
-      TipoDato,
-      Fecha, 
-      Diario, 
-      TipoPoliza, 
-      Folio, 
-      ConceptoPoliza,
-      NumMovto, 
-      AreaDeNegocio, 
-      CentroCosto, 
-      CodSegNegocio, 
-      SegmentoNegocio, 
-      CodCtaMayor, 
-      NomCtaMayor, 
-      CodSubCta, 
-      NomSubCta, 
-      CodSubSubCta, 
-      NomSubSubCta, 
-      CodSubSubSubCta, 
-      NomSubSubSubCta, 
-      CodigoCuenta, 
-      NombreCuenta,
-      Referencia, 
-      ConceptoMovimiento,
-      CuentaAnalitica,
-      Cargos, 
-      Abonos, 
-      ImporteNeto,
-      SaldoSemanal, 
-      SaldoMensual, 
-      SaldoAnual, 
-      Project, 
-      Comments
-    FROM
-    (
-      SELECT 
-        'Occidente' as Grupo,     
-          '{}' as Plaza,
-          'Financieros' as TipoDato,
-        DATE_PART('year', enc.date) as año, 
-        DATE_PART('month', enc.date) as mes,
-        DATE_PART('week', enc.date) as Semana,
-        enc.date as Fecha,
-        part.name as Usuario,
-        jou.name as Diario,
-        CASE 
-          WHEN enc.type = 'entry' THEN 'Asiento contable'
-          WHEN enc.type = 'out_invoice' THEN 'Factura de cliente'
-          WHEN enc.type = 'out_refund' THEN 'Nota de crédito de cliente'
-          WHEN enc.type = 'in_invoice' THEN 'Factura de proveedor'
-          WHEN enc.type = 'in_refund' THEN 'Nota de crédito de proveedor'
-          WHEN enc.type = 'out_receipt' THEN 'Recibo de ventas'
-          WHEN enc.type = 'in_receipt' THEN 'Recibo de compra'
-          ELSE enc.type
-        END AS TipoPoliza,
-        enc.id as Folio,
-        CASE
-           WHEN enc.ref = 'Sync Ecobro' THEN 'Abono'
-           ELSE enc.ref
-        END as ConceptoPoliza,
-        ROW_NUMBER() OVER (PARTITION BY enc.name) as NumMovto,
-
-        /*Falta definir las cuentas analiticas (una por movimiento) y las etiquetas analiticas (una o más por movimiento)*/
-        CASE
-          WHEN substring(ana.name, 1,1) = '1' THEN 'PABS'
-          WHEN substring(ana.name, 1,1) = '2' THEN 'Funeraria'
-          WHEN substring(ana.name, 1,1) = '3' THEN 'Panteon'
-          WHEN substring(ana.name, 1,2) = '98' THEN 'Apoyos'
-          WHEN substring(ana.name, 1,2) = '99' THEN 'Personales'
-          ELSE substring(ana.name, 1,2)
-        END as AreaDeNegocio,
-
-        CASE
-          WHEN substring(ana.name, 1,4) = '1001' THEN 'Administración'
-          WHEN substring(ana.name, 1,4) = '1002' THEN 'Cobranza'
-          WHEN substring(ana.name, 1,2) = '11' THEN 'Ventas'
-          WHEN substring(ana.name, 1,1) = '2' THEN 'Funerarias'
-          WHEN substring(ana.name, 1,2) = '98' THEN 'Apoyos'
-          WHEN substring(ana.name, 1,2) = '99' THEN 'Gastos personales'
-          ELSE substring(ana.name, 1,2)
-        END as CentroCosto,
-        substring(ana.name, 6, 99) as SegmentoNegocio,
-        substring(ana.name, 1,4) as CodSegNegocio,
-
-        /*CodCtaMayor*/
-        substring(acc.code,1,3) as CodCtaMayor,
-        CASE
-          WHEN substring(acc.name,1,3) = '601' THEN CASE
-                                WHEN substring(acc.code,5,2) = '01' THEN 'Caja'
-                                WHEN substring(acc.code,5,2) = '02' THEN 'Bancos'
-                                WHEN substring(acc.code,5,2) = '03' THEN 'RIF'
-                                WHEN substring(acc.code,5,2) = '00' THEN 'Programa de beneficio pabs'
-                                WHEN substring(acc.code,5,2) = '10' THEN 'Cooperativa de desarrollo'
-                                ELSE substring(acc.code,5,2)
-                              END
-          ELSE acc.name
-        END as NomCtaMayor,
-
-        /*CodSubCta*/
-        substring(acc.code,5,2) as CodSubCta,
-        CASE 
-          WHEN substring(acc.name,1,3) = '601' THEN acc.name
-          ELSE ''
-        END as NomSubCta,
-
-        /*CodSubSubCta*/
-        '' as CodSubSubCta,
-        '' as NomSubSubCta,
-
-        /*CodSubSubSubCta*/
-        '' as CodSubSubSubCta,
-        CASE
-          WHEN substring(acc.name,1,3) = '601' THEN acc.name
-          ELSE ''
-        END as NomSubSubSubCta,
-
-        substring(acc.code,1,3) as CodCtaMayorX,
-        CASE
-          WHEN substring(acc.code,1,3) = '004' THEN 'INGRESOS'
-          WHEN substring(acc.code,1,3) = '005' THEN 'COSTO DE VENTA'
-          WHEN substring(acc.code,1,3) = '006' THEN 'GASTOS DE OPERACIÓN'
-          WHEN substring(acc.code,1,3) = '007' THEN 'PRODUCTOS FINANCIEROS'
-          WHEN substring(acc.code,1,3) = '010' THEN 'ACTIVO CIRCULANTE'
-          WHEN substring(acc.code,1,3) = '015' THEN 'ACTIVO FIJO'
-          WHEN substring(acc.code,1,3) = '018' THEN 'ACTIVO DIFERIDO'
-          WHEN substring(acc.code,1,3) = '020' THEN 'PASIVO EXIGIBLE'
-          WHEN substring(acc.code,1,3) = '025' THEN 'PASIVO A LARGO PLAZO'
-          WHEN substring(acc.code,1,3) = '027' THEN 'INGRESOS POR REALIZAR Y COMPROMISOS DIFERIDOS'
-          WHEN substring(acc.code,1,3) = '028' THEN 'ANTICIPOS CLIENTES'
-          WHEN substring(acc.code,1,3) = '085' THEN 'OTROS INGRESOS Y OTROS GASTOS'
-          WHEN substring(acc.code,1,3) = '088' THEN 'IMPUESTOS CAUSADOS'
-          WHEN substring(acc.code,1,3) = '089' THEN 'RESERVAS Y RETIROS DE UTILIDADES'
-          WHEN substring(acc.code,1,4) = '1012' THEN 'Cajas en Tesorería'
-          ELSE substring(acc.code,1,3)
-        END as NomCtaMayorX,
-
-        substring(acc.code,5,2) as CodSubCtaX,
-        CASE
-          WHEN substring(acc.code,5,2) = '01' THEN 'Caja'
-          WHEN substring(acc.code,5,2) = '02' THEN 'Bancos'
-          WHEN substring(acc.code,5,2) = '03' THEN 'RIF'
-          WHEN substring(acc.code,5,2) = '00' THEN 'Programa de beneficio pabs'
-          WHEN substring(acc.code,5,2) = '10' THEN 'Cooperativa de desarrollo'
-          ELSE substring(acc.code,5,2)
-        END as NomSubCtaX,
-
-        substring(acc.code,8,99) as CodSubSubCtaX,
-        acc.name as NomSubSubCtaX,
-
-        '' as CodSubSubSubCtaX,
-        '' as NomSubSubSubCtaX,
-
-        acc.code as CodigoCuenta,
-        acc.name as NombreCuenta,
-        acc.name as NombreCuentaSAP,
-        /*Fin cuentas*/
-
-        mov.ref as Referencia,
-        COALESCE(mov.name,'Sin concepto') AS ConceptoMovimiento,
-        aaa.name AS CuentaAnalitica,
-        mov.Debit AS Cargos,
-        mov.Credit AS Abonos,
-        (mov.Debit - mov.Credit) as ImporteNeto,
-        0 as SaldoSemanal,
-        0 as SaldoMensual,
-        0 as SaldoAnual,
-        '' as Project,
-        mov.name as comments
-
-      FROM account_move AS enc
-      INNER JOIN account_move_line as mov on enc.id = mov.move_id
-      INNER JOIN res_users AS usr ON enc.create_uid = usr.id
-      INNER JOIN res_partner AS part ON usr.partner_id = part.id
-      LEFT JOIN account_analytic_account AS aaa ON mov.analytic_account_id = aaa.id
-      LEFT JOIN account_journal AS jou ON enc.journal_id = jou.id
-      LEFT JOIN account_account as acc on mov.account_id = acc.id
-      LEFT JOIN account_analytic_account as ana on mov.analytic_account_id = ana.id
-        WHERE enc.state = 'posted'
-        AND enc.company_id = {}
-        AND DATE_PART('year', enc.date) = 2021 
-        AND DATE_PART('month', enc.date) >= 6
-    ) as financieros""".format(company.name, company_id)
+    ) as financieros""".format(company.name, year, month, company_id)
     try:
       cr.execute(query)
       records = []
