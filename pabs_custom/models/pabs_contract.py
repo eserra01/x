b75095a8e23b5fa02a9adfad8234764275d9af65
jaimerodriguezpabs <jaime.rodriguez@pabsmr.org>
2022-10-20 14:17:04 -0500

    [ADD] Calculo de saldo en contratos cancelados fiscalmente y con traspaso v3

--- pabs_custom/models/pabs_contract.py
+++ pabs_custom/models/pabs_contract.py
@@ -548,7 +548,7 @@ class PABSContracts(models.Model):
         # No sumar traspasos cuando el contrato ya fue cancelado fiscalmente (tiene una factura con el producto "Cancelación fiscal")
         if rec.company_id.apply_taxes:
           cancelado_fiscalmente = self.env['account.move.line'].search_count([
-            ('partner_id', '=', rec.partner_id),
+            ('partner_id', '=', rec.partner_id.id),
             ('parent_state', '=', 'posted'),
             ('product_id.default_code', '=', 'DESC-0009')
           ])
