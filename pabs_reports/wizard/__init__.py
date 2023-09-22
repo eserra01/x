b9fc642b4533a421a9c589c68900d15f1cd37271
jaimerodriguezpabs <jaime.rodriguez@pabsmr.org>
2023-09-22 13:19:24 -0600

    [FIX] Reporte de efectividad

--- pabs_reports/wizard/__init__.py
+++ pabs_reports/wizard/__init__.py
@@ -16,3 +16,4 @@ from . import pabs_stock_push_and_pull_wizard
 from . import pabs_difference_between_amount_contracts_wizard
 from . import pabs_commissions_as
 from . import pabs_contratos_pagados
+from . import pabs_employee_effectiveness
