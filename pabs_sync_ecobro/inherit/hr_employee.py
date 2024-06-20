f127cbccb54be5516026c155d0d26ccfdea10f0e
eserrano-pabs <eduardo.serrano@pabsmr.org>
2024-06-20 17:39:43 -0600

    [ADD] actualizar ecobro_id de cobradores

--- pabs_sync_ecobro/inherit/hr_employee.py
+++ pabs_sync_ecobro/inherit/hr_employee.py
@@ -5,5 +5,5 @@ from odoo import api, fields, models
 class HREmployee(models.Model):
   _inherit = 'hr.employee'
 
-  ecobro_id = fields.Char(string='Ecobro ID')
+  ecobro_id = fields.Char(string='Ecobro ID', tracking=True)
   
