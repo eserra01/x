34aeaccc754c89ccb9e0d7e2ab9c45b74bf86c94
eserrano-pabs <eduardo.serrano@pabsmr.org>
2024-09-10 14:37:41 -0600

    [FIX] formato para campo de telefono

--- pabs_custom/static/src/js/field_validation.js
+++ pabs_custom/static/src/js/field_validation.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 
   // Solo números en campo de teléfono
   $("input[name='phone_toll']").keydown(function(event) {
-    if(isNaN(event.key) && event.key !== 'Backspace') {
+    if(isNaN(event.key) && event.key !== 'Backspace' && !event.ctrlKey) {
       event.preventDefault();
     }
   });
 
   $("input[name='phone']").keydown(function(event) {
-    if(isNaN(event.key) && event.key !== 'Backspace') {
+    if(isNaN(event.key) && event.key !== 'Backspace' && !event.ctrlKey) {
       event.preventDefault();
     }
   });
